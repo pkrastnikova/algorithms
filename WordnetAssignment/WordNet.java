820c4348a177f67e71d060fd719ea8f2ec084a60
Pepa <pkrastnikova@yahoo.com>
2018-10-20 21:16:16 +0300

    Add comments

--- WordnetAssignment/WordNet.java
+++ WordnetAssignment/WordNet.java
@@ -1,3 +1,20 @@
+/**
+ * Builds the WordNet digraph: each vertex v is an integer that represents a synset,
+ * and each directed edge v→w represents that w is a hypernym of v. One word can be part of
+ * several synsets.
+ * The WordNet digraph is a rooted DAG: it is acyclic and has one vertex—the root—
+ * that is an ancestor of every other vertex.
+ * 
+ * Provides methods for searching the distance between two nouns and their common ancestor that
+ * participates in the shortest ancestral path (SAP)
+ * 
+ * Dependencies: SAP.java, Digraph.java, Bag.java
+ * 
+ * @param synsets input synsets file
+ * @param hypernyms input hypernyms file
+ * @throws IllegalArgumentException when synsets or hypernyms file is null, or graph is not rooted
+ * @author pkrastnikova
+ */
 import java.util.HashMap;
 import java.util.Map;
 import edu.princeton.cs.algs4.Bag;
@@ -5,49 +22,44 @@ import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
 public class WordNet {
-	private Map<String, Bag<Integer>> wordSynsetsMap;
-	// maps "word -> synsets IDs", one word can be part of several synsets (IDs)
-	private Map<Integer, String> idSynsetMap; // maps "ID -> synset"
+	private Map<String, Bag<Integer>> wordSynsets; // maps "word -> synsets IDs"; one word can be part of several synsets
+	private Map<Integer, String> idSynset; // maps "ID -> synset"
 	private Digraph G;
 	private int graphsize;
 	private SAP paths;
 
-	// constructor takes the name of the two input files
+	// constructor takes the names of the two input files
 	public WordNet(String synsets, String hypernyms) {
 		if (synsets == null || hypernyms == null) {
 			throw new java.lang.IllegalArgumentException();
 		}
-		this.wordSynsetsMap = new HashMap<String, Bag<Integer>>();
-		this.idSynsetMap = new HashMap<Integer, String>();
+		wordSynsets = new HashMap<String, Bag<Integer>>();
+		idSynset = new HashMap<Integer, String>();
 		buildWordMap(synsets);
-		this.G = new Digraph(graphsize);
+		G = new Digraph(graphsize);
 		buildGraph(hypernyms);
 		if (!rootedDigraph1()) {
 			throw new java.lang.IllegalArgumentException("Graph not rooted");
 		}
-		this.paths = new SAP(G);
+		paths = new SAP(G);
 	}
 
-	/*
-	 * check G.outdegree(v) for each vertex if there's only one vertex with 0
-	 * outdegree (the root) -> rooted
+	/**
+	 * Checks outdegree(v) for each vertex;
+	 * If there's only one vertex with 0 outdegree (the root) -> rooted
 	 */
 	private boolean rootedDigraph1() {
 		int count = 0;
-		for (int id: this.idSynsetMap.keySet()){
+		for (int id: this.idSynset.keySet()){
 			if (G.outdegree(id) == 0) {count++;}
 		}
-		
-		//System.out.println("Outdegree entity: " + G.outdegree(38003));
-		//System.out.println("Rootcount: " + count);
-		return (count == 1);
-			
+		return (count == 1);	
 	}
 
-	/*
-	 * map word -> synset IDs (all synsets the word is part of)
-	 * map ID -> synset for each record of synsets file
-	 * set size of the map to be Digraph's size (number of vertices)
+	/**
+	 * Maps word -> IDs of synsets the word is part of
+	 * Maps ID -> synset for each record of synsets file
+	 * Sets size of the map to be Digraph's size (number of vertices)
 	 * @param filename synsets file
 	 */
 	private void buildWordMap(String filename) {
@@ -56,22 +68,24 @@ public class WordNet {
 			String line = synsets.readLine();
 			String[] fields = line.split("\\,");
 			int id = Integer.parseInt(fields[0]);
-			idSynsetMap.put(id, fields[1]);
+			idSynset.put(id, fields[1]);
 			String[] words = fields[1].split("\\ ");
-			for (String word : words) {
-				Bag<Integer> synsetsOfWord = wordSynsetsMap.get(word);
+			for (String word: words) {
+				Bag<Integer> synsetsOfWord = wordSynsets.get(word);
 				if (synsetsOfWord == null) {
-					wordSynsetsMap.put(word, synsetsOfWord = new Bag<Integer>());
+					wordSynsets.put(word, synsetsOfWord = new Bag<Integer>());
 				}
 				synsetsOfWord.add(id);
 			}
 		}
-		this.graphsize = wordSynsetsMap.size();
+		this.graphsize = wordSynsets.size();
 		synsets.close();
-
 	}
 
-	// build Digraph from hypernyms file (create edges)
+	/**
+	 * Builds Digraph from hypernyms file (creates edges between synset and its hypernyms)
+	 * @param filename hypernyms file
+	 */
 	private void buildGraph(String filename) {
 		In hypernyms = new In(filename);
 		while (hypernyms.hasNextLine()) {
@@ -86,20 +100,33 @@ public class WordNet {
 		hypernyms.close();
 	}
 
-	// returns all WordNet nouns
+	/**
+	 * @return all WordNet nouns
+	 */
 	public Iterable<String> nouns() {
-		return wordSynsetsMap.keySet();
+		return wordSynsets.keySet();
 	}
 
-	// is the word a WordNet noun?
+	/**
+	 * Checks if the word a WordNet noun
+	 * @param word a word
+	 * @return (@code true) if the word is a WordNet noun and (@code false) if it is not
+	 */
 	public boolean isNoun(String word) {
 		if (word == null) {
 			throw new java.lang.IllegalArgumentException();
 		}
-		return wordSynsetsMap.containsKey(word);
+		return wordSynsets.containsKey(word);
 	}
 
-	// distance between nounA and nounB (defined below)
+	/**
+	 * Computes the distance between nounA and nounB (number of edges from nounA to common
+	 * ancestor plus number of edges from nounB to common ancestor)
+	 * @param nounA a word
+	 * @param nounB a word
+	 * @throws IllegalArgumentException for null arguments or for words that are not in the graph
+	 * @return distance between nounA and nounB
+	 */
 	public int distance(String nounA, String nounB) {
 		if (nounA == null || nounB == null) {
 			throw new java.lang.IllegalArgumentException();
@@ -107,15 +134,18 @@ public class WordNet {
 		if (!isNoun(nounA) || !isNoun(nounB)) {
 			throw new java.lang.IllegalArgumentException("Not a WordNet noun");
 		}
-		Bag<Integer> idA = wordSynsetsMap.get(nounA); // get synsets IDs for nounA
-		Bag<Integer> idB = wordSynsetsMap.get(nounB); // get synsets IDs for nounB
-		return paths.length(idA, idB); // call length(Iterable<Integer> v,
-										// Interable<Integer> w)
+		Bag<Integer> idA = wordSynsets.get(nounA); // get synsets IDs of nounA
+		Bag<Integer> idB = wordSynsets.get(nounB); // get synsets IDs of nounB
+		return paths.length(idA, idB); // call length(Iterable<Integer> v, Interable<Integer> w)
 	}
 
-	/*
-	 * a synset (second field of synsets.txt) that is the common ancestor of
-	 * nounA and nounB in a shortest ancestral path (defined below)
+	/**
+	 * Finds a synset that is the common ancestor of nounA and nounB and is 
+	 * in the shortest ancestral path
+	 * @param nounA a word
+	 * @param nounB a word
+	 * @throws IllegalArgumentException for null arguments or for non-WordNet words
+	 * @return the synset, the common ancestor 
 	 */
 
 	public String sap(String nounA, String nounB) {
@@ -125,10 +155,10 @@ public class WordNet {
 		if (!isNoun(nounA) || !isNoun(nounB)) {
 			throw new java.lang.IllegalArgumentException("Not a WordNet noun");
 		}
-		Bag<Integer> idA = wordSynsetsMap.get(nounA);
-		Bag<Integer> idB = wordSynsetsMap.get(nounB);
+		Bag<Integer> idA = wordSynsets.get(nounA);
+		Bag<Integer> idB = wordSynsets.get(nounB);
 		int commonAncestor = paths.ancestor(idA, idB);
-		return idSynsetMap.get(commonAncestor);
+		return idSynset.get(commonAncestor);
 	}
 
 	// do unit testing of this class
@@ -136,20 +166,14 @@ public class WordNet {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		
 		 WordNet wordnet = new WordNet(args[0], args[1]);
 		 //System.out.println("Is digraph rooted? " + wordnet.rootedDigraph1());
 		 /*for (String noun: wordnet.nouns()) { System.out.println(noun);
 		 
-		 
 		 }*/
-		 
-
 		System.out.println("CA: " + wordnet.sap("municipality", "region"));
 		System.out.println("DIST: " + wordnet.distance("municipality",
 		 "region"));
-		
 		System.out.println("CA: " + wordnet.sap("harm", "increase"));
 		System.out.println("DIST: " + wordnet.distance("harm", "increase"));
 		System.out.println("CA: " + wordnet.sap("descent", "resistance"));
@@ -177,6 +201,5 @@ public class WordNet {
 		 System.out.println("CA: " + wordnet.sap("harm", "increase"));
 		 System.out.println("DIST: " + wordnet.distance("harm", "increase"));*/
 		 
-
 	}
 }
